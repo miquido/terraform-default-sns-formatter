7743e1defbf7060f020b5b65fa65a568e024dbc5
Marek Mościchowski <marek.moscichowski@miquido.com>
2025-10-13 10:29:13 +0200

    fix: SES formatter bounces

--- lambda/formatters/SESBounces.py
+++ lambda/formatters/SESBounces.py
@@ -7,16 +7,20 @@ class SESBounces(Formatter):
     @staticmethod
     def format(record):
         sns_object = json.loads(record)
+        try:
+            diagnostic = f"\n- Diagnostic:{sns_object['bounce']['bouncedRecipients'][0]['diagnosticCode']}"
+        except KeyError as e:
+            diagnostic = ""
         if sns_object['notificationType'] == 'Bounce':
             return {
                 'formatted': True,
-                'message': f'SES Bounce:\n- Mail: {sns_object['mail']['destination']}\n- Type: {sns_object['bounce']['bounceType']}\n- Diagnostic:{sns_object['bounce']['bouncedRecipients'][0]['diagnosticCode']}',
+                'message': f'SES Bounce:\n- Mail: {sns_object['mail']['destination']}\n- Type: {sns_object['bounce']['bounceType']}{diagnostic}',
                 'ignore': False,
             }
         if sns_object['notificationType'] == 'Complaint':
             return {
                 'formatted': True,
-                'message': f'SES Complaint:\n- Mail: {sns_object['mail']['destination']}\n- Type: {sns_object['complaint']['complaintFeedbackType']}\n- Diagnostic:{sns_object['bounce']['bouncedRecipients'][0]['diagnosticCode']}',
+                'message': f'SES Complaint:\n- Mail: {sns_object['mail']['destination']}\n- Type: {sns_object['complaint']['complaintFeedbackType']}{diagnostic}',
                 'ignore': False,
             }
         return {
