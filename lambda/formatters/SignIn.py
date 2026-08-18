13f5bf851931e330b4c6295060c1350f9497e3ef
Marek Mościchowski <marek.moscichowski@miquido.com>
2026-08-18 07:47:15 +0200

    feat: Added more fields to root login default formatter

--- lambda/formatters/SignIn.py
+++ lambda/formatters/SignIn.py
@@ -8,9 +8,28 @@ class SignIn(Formatter):
     def format(record):
         sns_object = json.loads(record)
         if sns_object['source'] == 'aws.signin':
+            detail = sns_object['detail']
+            lines = [
+                'ROOT sign in',
+                f'- {detail['eventName']}',
+                f'- IP: {detail['sourceIPAddress']}',
+            ]
+
+            account = sns_object.get('account')
+            if account:
+                lines.append(f'- Account: {account}')
+
+            login_result = detail.get('responseElements', {}).get('ConsoleLogin')
+            if login_result:
+                lines.append(f'- Login result: {login_result}')
+
+            mfa_used = detail.get('additionalEventData', {}).get('MFAUsed')
+            if mfa_used:
+                lines.append(f'- MFA used: {mfa_used}')
+
             return {
                 'formatted': True,
-                'message': f'ROOT sign in \n- {sns_object['detail']['eventName']}\n- IP: {sns_object['detail']['sourceIPAddress']}',
+                'message': '\n'.join(lines),
                 'ignore': False,
             }
 
